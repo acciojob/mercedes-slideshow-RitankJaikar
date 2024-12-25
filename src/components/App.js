d923c8c467eb9eba3ff152382b01f3d564381714
RitankJaikar <ritank.jaikar@gmail.com>
2024-12-25 13:28:29 +0530

    submit

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,9 @@ import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
 const imgUrl = [
-  "https://imgd.aeplcdn.com/1920x1080/n/cw/ec/178535/c-class-exterior-right-front-three-quarter.jpeg?isig=0&q=80&q=80",
-  "https://stimg.cardekho.com/images/carexteriorimages/930x620/Mercedes-Benz/GLS/9791/1704772236530/front-left-side-47.jpg",
-  "https://imgd.aeplcdn.com/642x336/n/cw/ec/162929/e-class-exterior-right-front-three-quarter-2.jpeg?isig=0&q=80"
+  "https://www.mercedes-benz.com/en/vehicles/mercedes-eq/eqs-suv/_jcr_content/root/verticalgallery_copy/parsys/verticalgalleryitem/image/MQ6-0-image-20220419101504/01-mercedes-benz-the-new-eqs-suv-x296-2021-1080x1350.jpeg",
+  "https://www.mercedes-benz.com/en/vehicles/mercedes-eq/eqs-suv/_jcr_content/root/verticalgallery_copy/parsys/verticalgalleryitem_546277676/image/MQ6-0-image-20220819111550/02-mercedes-benz-the-new-eqs-suv-x296-2021-1680x1680.jpeg",
+  "https://www.mercedes-benz.com/en/vehicles/mercedes-eq/eqs-suv/_jcr_content/root/verticalgallery_copy/parsys/verticalgalleryitem_354875624/image/MQ6-0-image-20220419101505/03-mercedes-benz-the-new-eqs-suv-x296-2021-1680x1680.jpeg"
 ]
 
 const App = () => {
